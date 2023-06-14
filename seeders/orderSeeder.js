e2d612d9333bbb46d5e42645bf9440c6b8942656
JuanEstevess <jignacioesteves@gmail.com>
2023-06-14 16:23:34 -0300

    order seeder

---
+++ seeders/orderSeeder.js
@@ -0,0 +1,23 @@
+const { Order } = require("../models");
+
+module.exports = async () => {
+  const orders = [
+    {
+      products: {
+        "product.id": "1",
+        "product.name": "#DIPA",
+        qty: 3,
+        price: 20,
+      },
+      subTotalPrice: 24,
+      taxes: 2,
+      totalAmount: 26,
+      status: "pending",
+      address: "SiempreViva 3333",
+      userId: 2,
+    },
+  ];
+
+  await Order.bulkCreate(orders);
+  console.log("[Database] Se corrió el seeder de Order.");
+};
--- seeders/runAllSeeders.js
+++ seeders/runAllSeeders.js
@@ -4,6 +4,7 @@ async function runAllSeeders() {
   await require("./categorySeeder")();
   await require("./productsSeeder")();
   await require("./userSeeder")();
+  await require("./orderSeeder")();
 
   console.log("[Database] ¡Los datos de prueba fueron insertados!");
 }
