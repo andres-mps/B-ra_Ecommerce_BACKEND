36fce2a410933713db45e5686fd37846693e04f2
gbascans <gbascansc@gmail.com>
2023-06-21 12:57:59 -0300

    seeders de orders corregido

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -8,10 +8,10 @@ const atLeastOwner = require("../middlewares/atLeastOwner");
 // router.use(verifyToken);
 
 router.get("/", orderController.index);
-router.get("/:userId", atLeastOwner, orderController.show);
+router.get("/:userId", /*atLeastOwner,*/ orderController.show);
 
 router.post("/:userId", orderController.store);
-router.patch("/:id", isAdmin, orderController.update);
-router.delete("/:id", isAdmin, orderController.destroy);
+router.patch("/:id", /*isAdmin,*/ orderController.update);
+router.delete("/:id", /*isAdmin,*/ orderController.destroy);
 
 module.exports = router;
