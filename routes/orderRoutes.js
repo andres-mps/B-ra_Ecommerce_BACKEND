4acab87aa513f5bcb8deae5e2cbf5d57a77e2b99
gbascans <gbascansc@gmail.com>
2023-06-21 20:03:28 -0300

    Algunas verificaciones a orders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,13 +5,13 @@ const verifyToken = require("../middlewares/verifyToken");
 const isAdmin = require("../middlewares/isAdmin");
 const atLeastOwner = require("../middlewares/atLeastOwner");
 
-// router.use(verifyToken);
+router.use(verifyToken);
 
 router.get("/", orderController.index);
-router.get("/:userId", /*atLeastOwner,*/ orderController.show);
+router.get("/:userId", atLeastOwner, orderController.show);
 
 router.post("/:userId", orderController.store);
-router.patch("/:id", /*isAdmin,*/ orderController.update);
-router.delete("/:id", /*isAdmin,*/ orderController.destroy);
+router.patch("/:id", isAdmin, orderController.update);
+router.delete("/:id", isAdmin, orderController.destroy);
 
 module.exports = router;
