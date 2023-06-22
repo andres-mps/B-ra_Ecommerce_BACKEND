f372a91e7959412b62137352a239dd80ed4e6c4f
JuanEstevess <jignacioesteves@gmail.com>
2023-06-22 14:35:15 -0300

    Ajustes en rutas y controlador de categorias

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const { Category, Product } = require("../models");
+const formidable = require("formidable");
 
 async function index(req, res) {
   const categories = await Category.findAll({
@@ -20,6 +21,18 @@ async function show(req, res) {
     res.json({ type: "err", content: "No existen productos para la categoria seleccionada" });
   }
 }
+async function showCategory(req, res) {
+  const params = req.params.category;
+  try {
+    const category = await Category.findOne({
+      where: { slug: params },
+    });
+    res.json(category);
+  } catch {
+    //const products = await Product.findAll();
+    res.json({ type: "err", content: "No existen productos para la categoria seleccionada" });
+  }
+}
 
 async function create(req, res) {
   const category = await Category.create({
@@ -34,16 +47,33 @@ async function store(req, res) {}
 async function edit(req, res) {}
 
 async function update(req, res) {
-  const id = req.params.id;
-  const { name, image } = req.body;
+  const categoryId = req.params.category;
 
   try {
-    await Category.update({ name, image }, { where: { id } });
+    const form = formidable({
+      multiples: true,
+      uploadDir: __dirname + "/../public/img",
+      keepExtensions: true,
+    });
 
-    res.json({ message: "Categoría actualizada exitosamente" });
+    form.parse(req, async (err, fields, files) => {
+      const { name, active } = fields;
+      console.log(files);
+      const category = await Category.findByPk(categoryId);
+      if (!category) {
+        return res.json({ error: "Category not found" });
+      }
+
+      name && name !== category.name && (category.name = name);
+      active && active !== category.active && (category.active = active);
+
+      files.image && files.image !== category.image && (category.image = files.image.newFilename);
+
+      await category.save();
+      res.json(category);
+    });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error al actualizar la categoría" });
+    return res.status(400).json({ message: error.message });
   }
 }
 
@@ -65,6 +95,7 @@ async function destroy(req, res) {
 module.exports = {
   index,
   show,
+  showCategory,
   create,
   store,
   edit,
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -5,8 +5,9 @@ const categoryController = require("../controllers/categoryController");
 router.get("/", categoryController.index);
 //Dejarla como ultima!!! sino parametriza antes de entrar a las otras opciones de ruta luego de products
 router.get("/:category", categoryController.show);
+router.get("/admin/:category", categoryController.showCategory);
 router.post("/create", categoryController.create);
-router.patch("/:id", categoryController.update);
+router.patch("/admin/update/:category", categoryController.update);
 router.delete("/:id", categoryController.destroy);
 
 module.exports = router;
