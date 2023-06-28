455df0bb4353674a2e9030cf14d9d5b258d03dd8
andres-mps <andresmps@gmail.com>
2023-06-28 15:28:39 -0300

    pg dialect

--- models/index.js
+++ models/index.js
@@ -1,12 +1,32 @@
 const { Sequelize } = require("sequelize");
 
+// const sequelizeOptions = {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   dialect: process.env.DB_CONNECTION,
+//   logging: false,
+// };
+// if (process.env.DB_CONNECTION === "postgres") {
+//   sequelizeOptions.dialectModule = require("pg");
+// }
+// const sequelize = new Sequelize(
+//   process.env.DB_DATABASE,
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASSWORD,
+//   sequelizeOptions,
+// );
+
+// TODO - boorrar si funciona lo anterior
 const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
+  sequelizeOptions,
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_CONNECTION,
+    port: process.env.DB_PORT,
+    dialectModule: require("pg"),
     logging: false, // Para que no aparezcan mensajes en consola.
   },
 );
