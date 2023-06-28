dd381584f31635f49bb616ca907188b844793df1
andres-mps <andresmps@gmail.com>
2023-06-28 15:37:08 -0300

    index models

--- models/index.js
+++ models/index.js
@@ -1,21 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-// const sequelizeOptions = {
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   dialect: process.env.DB_CONNECTION,
-//   logging: false,
-// };
-// if (process.env.DB_CONNECTION === "postgres") {
-//   sequelizeOptions.dialectModule = require("pg");
-// }
-// const sequelize = new Sequelize(
-//   process.env.DB_DATABASE,
-//   process.env.DB_USERNAME,
-//   process.env.DB_PASSWORD,
-//   sequelizeOptions,
-// );
-
 // TODO - boorrar si funciona lo anterior
 const sequelize = new Sequelize(
   process.env.DB_DATABASE,
