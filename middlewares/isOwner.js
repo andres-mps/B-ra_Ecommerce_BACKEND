f84c60a84c58558857b6e9d06b94fd46b9e16c7f
gbascans <gbascansc@gmail.com>
2023-06-28 17:44:53 -0300

    middlewares del back

--- middlewares/isOwner.js
+++ middlewares/isOwner.js
@@ -1,4 +1,4 @@
 function isOwner(req, res, next) {
-  return req.auth.id === Number(req.params.id) ? next() : res.json("usuario no autorizado");
+  return req.auth.id === req.params.id ? next() : res.json("el error es en isOwner");
 }
 module.exports = isOwner;
